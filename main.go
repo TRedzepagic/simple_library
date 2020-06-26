3745074b5e26ffe83b9ca847c8378741368286cc
TRedzepagic <tarik.redzepagic.2012@gmail.com>
2020-06-26 10:02:54 +0200

    Getting books now possible

--- main.go
+++ main.go
@@ -15,11 +15,11 @@ type author struct {
 }
 
 type book struct {
-	ISBN   int    `json:"ISBN"`
-	Title  string `json:"randnumber"`
-	Pages  int    `json:"pages"`
-	Year   int    `json:"year"`
-	Author author `json:"author"`
+	ISBN   int     `json:"ISBN"`
+	Title  string  `json:"randnumber"`
+	Pages  int     `json:"pages"`
+	Year   int     `json:"year"`
+	Author *author `json:"author"`
 }
 
 func getBooks(w http.ResponseWriter, r *http.Request) {
@@ -45,8 +45,8 @@ func deleteBook(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	// Mock data initialization
-	testbook1 := book{ISBN: 111111, Title: "Cooking 1", Pages: 240, Year: 2003, Author: author{Name: "Tarik", Surname: "Redzepagic"}}
-	testbook2 := book{ISBN: 222222, Title: "Farming 1", Pages: 300, Year: 2005, Author: author{Name: "Kirat", Surname: "Pagicredz"}}
+	testbook1 := book{ISBN: 111111, Title: "Cooking 1", Pages: 240, Year: 2003, Author: &author{Name: "Tarik", Surname: "Redzepagic"}}
+	testbook2 := book{ISBN: 222222, Title: "Farming 1", Pages: 300, Year: 2005, Author: &author{Name: "Kirat", Surname: "Pagicredz"}}
 	library[testbook1.ISBN] = testbook1
 	library[testbook2.ISBN] = testbook2
 
